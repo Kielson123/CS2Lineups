00c794bacdedc75d59d7d6f37adbd2d8efe27e0d
Kielson <gracjanprzymus12@gmail.com>
2025-04-30 20:02:36 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+//http-server -c1 -p80
+
+function addLineups() {
+    let tags = document.getElementsByTagName("*")
+    for (let i = 0; i < tags.length; i++) {
+        let elmnt = tags[i] 
+        let file = elmnt.getAttribute("include-lineup")
+        if (file) {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    
+                    elmnt.removeAttribute("include-lineup");
+                    addLineups();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+
+            return;
+        }
+    }
+}
