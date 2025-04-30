df8711ec27d04fac2bb4ec9d0e936ffbb00b545d
Kielson <gracjanprzymus12@gmail.com>
2025-05-01 00:07:40 +0200

    Added first video

--- main.js
+++ main.js
@@ -31,15 +31,15 @@ function addLineups() {
 function openTab(event, tabName) {
     let tabcontent = document.getElementsByClassName("tab-content");
     for (let i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
   
     let tablinks = document.getElementsByClassName("tab-links");
     for (let i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
   
-    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tabName).style.display = "flex";
     event.currentTarget.className += " active";
 }
 
