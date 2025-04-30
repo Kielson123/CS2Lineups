4684dda0fbe0f5a795b7514c0b72b60eda8f46fc
Kielson <gracjanprzymus12@gmail.com>
2025-04-30 20:13:50 +0200

    Changed project structure

--- main.js
+++ main.js
@@ -27,3 +27,20 @@ function addLineups() {
         }
     }
 }
+
+function openTab(event, tabName) {
+    let tabcontent = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    let tablinks = document.getElementsByClassName("tab-links");
+    for (let i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
